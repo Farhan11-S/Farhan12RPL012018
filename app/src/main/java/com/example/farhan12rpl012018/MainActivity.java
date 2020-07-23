8a4653578fae082bca8263a165fe3ef6970ca184
Farhan11-S <farhanxrpl2018smkrus@gmail.com>
2020-07-23 21:24:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/farhan12rpl012018/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.farhan12rpl012018;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
